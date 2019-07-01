8554f7dac1e8661a7c3cd8b0210f6ec2fb4f9c19
Masteholtet <eirik.persson.masteholtet@nav.no>
2019-07-01 15:33:33 +0200

    Kan nå liste opp 2 eksempelmaler samt be om innholdet i en spesifikk mal

--- src/main/java/no/nav/familie/dokumentgenerator/demo/controller/TemplateController.java
+++ src/main/java/no/nav/familie/dokumentgenerator/demo/controller/TemplateController.java
@@ -1,4 +1,50 @@
 package no.nav.familie.dokumentgenerator.demo.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.jknack.handlebars.Template;
+import no.nav.familie.dokumentgenerator.demo.model.TemplateService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RestController
 public class TemplateController {
+
+    private final TemplateService templateManagementService;
+
+    public TemplateController(TemplateService templateManagementService) {
+        this.templateManagementService = templateManagementService;
+    }
+
+    @GetMapping("/maler")
+    public List<String> getAllTemplateNames() {
+        try {
+            return templateManagementService.listAllTemplateNames();
+        } catch (IOException e) {
+            System.out.println("Kunne ikke finne noen maler!");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping(value = "maler/{name}", produces="text/html")
+    public String getTemplate(@PathVariable String name) {
+        try {
+            Template template = templateManagementService.getTemplate(name);
+            URL path = templateManagementService.getJsonPath(name + ".json");
+            JsonNode jsonNode = templateManagementService.readJsonFile(path);
+            return template.apply(templateManagementService.getContext(jsonNode));
+        } catch (IOException | com.github.jknack.handlebars.HandlebarsException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
--- src/main/java/no/nav/familie/dokumentgenerator/demo/model/TemplateService.java
+++ src/main/java/no/nav/familie/dokumentgenerator/demo/model/TemplateService.java
@@ -1,4 +1,90 @@
 package no.nav.familie.dokumentgenerator.demo.model;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.jknack.handlebars.Context;
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.JsonNodeValueResolver;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.context.FieldValueResolver;
+import com.github.jknack.handlebars.context.JavaBeanValueResolver;
+import com.github.jknack.handlebars.context.MapValueResolver;
+import com.github.jknack.handlebars.context.MethodValueResolver;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TemplateService {
+    private Handlebars handlebars;
+
+
+    public Handlebars getHandlebars() {
+        return handlebars;
+    }
+
+    public void setHandlebars(Handlebars handlebars) {
+        this.handlebars = handlebars;
+    }
+
+    @PostConstruct
+    public void loadHandlebarTemplates() {
+        TemplateLoader loader = new ClassPathTemplateLoader("/templates", ".hbs");
+        setHandlebars(new Handlebars(loader));
+    }
+
+    public List<String> listAllTemplateNames() throws IOException {
+        List<String> templateNames = new ArrayList<>();
+        File folder = new ClassPathResource("templates").getFile();
+        File[] listOfFiles = folder.listFiles();
+
+        if (listOfFiles == null) {
+            return null;
+        }
+
+        for (File file : listOfFiles) {
+            templateNames.add(file.getName());
+        }
+        return templateNames;
+    }
+
+    public Template getTemplate(String templateName) throws IOException {
+        return this.getHandlebars().compile(templateName);
+    }
+
+    public URL getJsonPath(String templateName) {
+        return ClassLoader.getSystemResource("json/" + templateName);
+    }
+
+    public JsonNode readJsonFile(URL path) {
+        if (path != null) {
+            ObjectMapper mapper = new ObjectMapper();
+
+            try {
+                return mapper.readTree(new File(path.toURI()));
+            } catch (IOException | URISyntaxException e) {
+                System.out.println("Could not find JSON file!");
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public Context getContext(JsonNode model) {
+        return Context
+                .newBuilder(model)
+                .resolver(JsonNodeValueResolver.INSTANCE, JavaBeanValueResolver.INSTANCE, FieldValueResolver.INSTANCE,
+                        MapValueResolver.INSTANCE,
+                        MethodValueResolver.INSTANCE)
+                .build();
+    }
 }
