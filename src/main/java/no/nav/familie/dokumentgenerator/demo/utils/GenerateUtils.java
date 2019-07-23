76e13f3c94a37594a2e4677cf42179268a66ac70
Peter Rydberg <hmp.rydberg@gmail.com>
2019-07-23 13:18:32 +0200

    Removed unused imports

--- src/main/java/no/nav/familie/dokumentgenerator/demo/utils/GenerateUtils.java
+++ src/main/java/no/nav/familie/dokumentgenerator/demo/utils/GenerateUtils.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
