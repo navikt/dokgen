30576860ed66729ac201ef1672815f6874a588dd
Nina Kjekstad <nina.kjekstad@nav.no>
2020-08-18 16:23:35 +0200

    Legg til interceptor og oppdater logback-filer så de fanger opp info-innslag fra appen

---
+++ src/main/java/no/nav/dokgen/configuration/WebConfig.java
@@ -0,0 +1,19 @@
+package no.nav.dokgen.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoggerInterceptor loggerInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggerInterceptor);
+    }
+
+}
