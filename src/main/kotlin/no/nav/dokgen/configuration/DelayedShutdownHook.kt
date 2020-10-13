dc1810afcde80cef35142ad02dfe50a8dbb5b6c2
Meng Zhu <meng.zhu@nav.no>
2020-10-13 09:52:44 +0200

    fix pr comments

--- src/main/kotlin/no/nav/dokgen/configuration/DelayedShutdownHook.kt
+++ src/main/kotlin/no/nav/dokgen/configuration/DelayedShutdownHook.kt
@@ -1,5 +1,7 @@
 package no.nav.dokgen.configuration
 
+import no.nav.dokgen.services.TemplateService
+import org.slf4j.LoggerFactory
 import org.springframework.context.ConfigurableApplicationContext
 
 class DelayedShutdownHook(private val applicationContext: ConfigurableApplicationContext) : Thread() {
@@ -9,10 +11,13 @@ class DelayedShutdownHook(private val applicationContext: ConfigurableApplicatio
             // https://nav-it.slack.com/archives/C5KUST8N6/p1543497847341300
             sleep(5000L)
         } catch (e: InterruptedException) {
-            e.printStackTrace()
+            LOG.error("En feil med shutdown oppstod", e)
         }
         applicationContext.close()
         super.run()
     }
 
+    companion object {
+        private val LOG = LoggerFactory.getLogger(DelayedShutdownHook::class.java)
+    }
 }
