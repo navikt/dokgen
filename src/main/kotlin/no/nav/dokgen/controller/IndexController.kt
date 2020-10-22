6f5522ea38a78e0ffc486f9db25e5ed87ce10c16
Tor Nærland <tor.nerland@nav.no>
2020-10-22 13:16:29 +0200

    Bumper springfox fra 2.9.0 til 3.0.0 iht. http://springfox.github.io/springfox/docs/current/#migrating-from-existing-2-x-version
    Legger til redirect for swagger-ui fra gammel til ny location i IndexController

--- src/main/kotlin/no/nav/dokgen/controller/IndexController.kt
+++ src/main/kotlin/no/nav/dokgen/controller/IndexController.kt
@@ -7,30 +7,32 @@ import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import springfox.documentation.swagger2.web.Swagger2Controller
 import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 class IndexController(
     val servletRequest: HttpServletRequest
 ) {
     @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = ["/"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(value = ["/"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun listTemplates(): EntityModel<IndexResource> {
-        val link = Link.of(servletRequest.requestURL.toString() + "swagger-ui.html")
+        val link = Link.of(servletRequest.requestURL.toString() + "swagger-ui/")
             .withRel("swagger-ui")
         return EntityModel.of(
             IndexResource("dokgen"),
             linkTo(methodOn(TemplateController::class.java).listTemplates())
                 .withRel("templates"),
-            link,
-            linkTo(
-                methodOn(Swagger2Controller::class.java)
-                    .getDocumentation("default", servletRequest)
-            ).withRel("swagger-doc")
+            link
         )
+    }
+
+    @GetMapping("/swagger-ui.html")
+    fun swaggerRedirect(response: HttpServletResponse) {
+        response.setHeader("Location", "/swagger-ui/")
+        response.status = 302
     }
 }
