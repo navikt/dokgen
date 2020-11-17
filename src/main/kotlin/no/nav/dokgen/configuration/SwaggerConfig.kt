910f3f3e39e74abb68776e4494fd7860efbaa777
tnarland <47184872+tnarland@users.noreply.github.com>
2020-11-17 16:41:41 +0100

    Bumper springfox fra 2.9.2 til 3.0.0
    iht. http://springfox.github.io/springfox/docs/current/#migrating-from-existing-2-x-version
    
    Legger til redirect for swagger-ui fra gammel til ny location i IndexController

--- src/main/kotlin/no/nav/dokgen/configuration/SwaggerConfig.kt
+++ src/main/kotlin/no/nav/dokgen/configuration/SwaggerConfig.kt
@@ -10,11 +10,9 @@ import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @Configuration
-@EnableSwagger2
 class SwaggerConfig {
     @Bean
     fun api(): Docket {
