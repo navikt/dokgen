b8147b5bebe27c5f7f5de14e70642a2380095b18
Michal J. Sladek <mrsladek@users.noreply.github.com>
2024-03-13 11:40:46 +0100

    Fjerner logging av *Using fallback font LiberationSans for base font xyz* (#346)

--- up.sh
+++ up.sh
@@ -3,4 +3,4 @@
 docker stop familie-dokgen; docker rm familie-dokgen
 mvn -B -Dfile.encoding=UTF-8 -DinstallAtEnd=true -DdeployAtEnd=true  -DskipTests clean install
 docker build -t navikt/dokgen .
-docker run -p 7281:8080 -d --name familie-dokgen navikt/dokgen
+docker run -p 7281:8080 -d --name familie-dokgen -e SPRING_PROFILES_ACTIVE=dev navikt/dokgen
